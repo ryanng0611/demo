08957c5364760bb38076e0cb41058276f2265af9
Snappymob <snappymob@Snappymobs-MacBook-Air.local>
2024-02-22 13:58:48 +0800

    USER-AUTH-2: Implement signIn() endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthUserDto } from './dto/auth-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService){}
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    signIn(@Body() authUserDto: AuthUserDto){
+        return this.authService.signIn(authUserDto.username, authUserDto.password);
+    }
+}
