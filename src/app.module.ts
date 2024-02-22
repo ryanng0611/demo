224da8dc20bc37ccbf3cc28f6ce5ff4592ac20ce
Snappymob <snappymob@Snappymobs-MacBook-Air.local>
2024-02-22 16:14:34 +0800

    DOCKER: Create TypeOrmModule to establish connection with PostgreSQL DB docker image

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres_db',
+      port: 5432,
+      username: 'demo_user',
+      password: 'demo_password',
+      database: 'demo_db',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
