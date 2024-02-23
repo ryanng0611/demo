cba02cebc28b752bc3bd247c265659091c68e0d2
Snappymob <snappymob@Snappymobs-MacBook-Air.local>
2024-02-23 10:34:09 +0800

    Modify user.entity.ts to host users table congifguration

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity("users")
 export class User {
+    @PrimaryGeneratedColumn()
     userId: number;
+
+    @Column()
     username: string;
+
+    @Column()
     password: string;
 }
